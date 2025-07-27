967923e00fddcc1b4b4c362fe1f7550fa8079659
Shikha <shikha@Shikhas-MacBook-Air.local>
2025-07-27 14:01:20 +0530

    Initial commit

---
+++ src/main/java/com/biharirasoi/bihari_rasoi/BihariRasoiApplication.java
@@ -0,0 +1,13 @@
+package com.biharirasoi.bihari_rasoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BihariRasoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BihariRasoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biharirasoi/bihari_rasoi/BihariRasoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biharirasoi.bihari_rasoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BihariRasoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
